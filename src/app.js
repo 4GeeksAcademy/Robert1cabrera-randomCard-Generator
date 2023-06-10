42b3445f8cf825d5336a1cb4e59bb36a2982279b
Robert <Robert1cabrera@gmail.com>
2023-06-10 01:04:06 +0000

    just need to style

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suites = ["♦", "♥", "♠", "♣"];
+  const cardnumbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  // function randomNum(numbers) {
+  //   return Math.floor(Math.random() * numbers.length);
+  // }
+
+  function randomCard(arr) {
+    return Math.floor(Math.random() * arr.length);
+  }
+  let suite = suites[randomCard(suites)];
+  let num = cardnumbers[randomCard(cardnumbers)];
+  let top = document.querySelector("#top");
+  top.innerHTML = suite;
+  let number = document.querySelector("#num");
+  number.innerHTML = num;
+  let bottom = document.querySelector("#bottom");
+  bottom.innerHTML = suite;
 };
